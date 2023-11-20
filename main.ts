2ed64056b597a61c6f07dc90af43a2db0d4e2346
Celvintheprogrammer <125273062+Celvintheprogrammer@users.noreply.github.com>
2023-11-20 13:42:16 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,22 +8,27 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
         bitbot.rotatems(BBRobotDirection.Right, 60, 500)
     }
+    if (receivedNumber == 3) {
+    	
+    }
 })
 input.onButtonPressed(Button.A, function () {
     if (input.buttonIsPressed(Button.A) && modus == 0) {
         radio.sendNumber(1)
-        modus = 1
+        telle_opp += 1
+    }
+    if (input.buttonIsPressed(Button.A) && modus == 1) {
+        radio.sendNumber(4)
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    if (input.buttonIsPressed(Button.A) && modus == 0) {
+    if (input.buttonIsPressed(Button.AB) && modus == 0) {
         radio.sendNumber(0)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (input.buttonIsPressed(Button.A) && modus == 0) {
+    if (input.buttonIsPressed(Button.B) && modus == 0) {
         radio.sendNumber(2)
-        telle_opp = 0
     }
 })
 let telle_opp = 0
@@ -34,9 +39,16 @@ radio.setGroup(101)
 bitbot.select_model(BBModel.XL)
 bitbot.go(BBDirection.Forward, 60)
 let weeewooweeewoo = 40
+modus = 0
 loops.everyInterval(500, function () {
-    if (true) {
-    	
+    if (telle_opp < 2 && modus == 0) {
+        telle_opp += -1
+    }
+    if (telle_opp == 2 && modus == 0) {
+        modus = 1
+    }
+    if (telle_opp > 2 && modus == 0) {
+        telle_opp = 2
     }
 })
 basic.forever(function () {
