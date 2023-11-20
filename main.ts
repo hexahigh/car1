d248aec57231bd1659f7e30800a3385534443e10
Celvintheprogrammer <125273062+Celvintheprogrammer@users.noreply.github.com>
2023-11-20 12:56:06 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,5 +18,24 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
 })
+let wooweeewooweee = 0
 bitbot.bbEnableBluetooth(BBBluetooth.btEnable)
 radio.setGroup(101)
+bitbot.select_model(BBModel.XL)
+bitbot.go(BBDirection.Forward, 60)
+let weeewooweeewoo = 40
+basic.forever(function () {
+    if (wooweeewooweee == 0) {
+        if (bitbot.readLight(BBLightSensor.Left) + bitbot.readLight(BBLightSensor.Right) > weeewooweeewoo) {
+            if (bitbot.readLight(BBLightSensor.Left) > bitbot.readLight(BBLightSensor.Right)) {
+                bitbot.rotate(BBRobotDirection.Left, 60)
+            } else if (bitbot.readLight(BBLightSensor.Left) < bitbot.readLight(BBLightSensor.Right)) {
+                bitbot.rotate(BBRobotDirection.Right, 60)
+            }
+        } else {
+            bitbot.go(BBDirection.Forward, 60)
+        }
+    } else if (wooweeewooweee == 1) {
+    	
+    }
+})
