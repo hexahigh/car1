1193a954d2bae4d656f99bd14b7ed3a3192efc50
Boof <97455552+hexahigh@users.noreply.github.com>
2023-11-20 13:51:54 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -34,28 +34,11 @@ input.onButtonPressed(Button.B, function () {
 })
 let telle_opp = 0
 let modus = 0
-let wooweeewooweee = 0
 bitbot.bbEnableBluetooth(BBBluetooth.btEnable)
 radio.setGroup(101)
 bitbot.select_model(BBModel.XL)
-let weeewooweeewoo = 40
 loops.everyInterval(500, function () {
     if (true) {
-    	
-    }
-})
-basic.forever(function () {
-    if (wooweeewooweee == 0) {
-        if (bitbot.readLight(BBLightSensor.Left) + bitbot.readLight(BBLightSensor.Right) > weeewooweeewoo) {
-            if (bitbot.readLight(BBLightSensor.Left) > bitbot.readLight(BBLightSensor.Right)) {
-                bitbot.rotate(BBRobotDirection.Left, 60)
-            } else if (bitbot.readLight(BBLightSensor.Left) < bitbot.readLight(BBLightSensor.Right)) {
-                bitbot.rotate(BBRobotDirection.Right, 60)
-            }
-        } else {
-            bitbot.go(BBDirection.Forward, 60)
-        }
-    } else if (wooweeewooweee == 1) {
     	
     }
 })
